cc8de3676c2be6362df71b1cc7f520aee3bebf98
elienay <elienayhenriquesouza@outlook.com>
2026-06-01 20:09:48 -0300

    atualizado hora do pesadelo

--- filmes_db.sql
+++ filmes_db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.3-2.fc44
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 26/05/2026 às 02:20
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+-- Host: localhost
+-- Tempo de geração: 01/06/2026 às 23:07
+-- Versão do servidor: 11.8.6-MariaDB
+-- Versão do PHP: 8.5.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,7 +35,7 @@ CREATE TABLE `filmes` (
   `nota` decimal(3,1) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_uca1400_ai_ci;
 
 --
 -- Despejando dados para a tabela `filmes`
@@ -45,7 +45,8 @@ INSERT INTO `filmes` (`id`, `nome`, `diretor`, `data_lancamento`, `nota`, `creat
 (1, 'O Poderoso Chefão (1972)', 'Francis Ford Coppola', '1972-03-24', 9.2, '2026-05-26 00:01:45', '2026-05-26 00:01:47'),
 (2, 'Interestelar', 'Christopher Nolan', '2014-11-06', 8.6, '2026-05-26 00:01:45', '2026-05-26 00:01:45'),
 (3, 'Parasita', 'Bong Joon-ho', '2019-05-30', 8.5, '2026-05-26 00:01:45', '2026-05-26 00:14:07'),
-(6, 'Planeta dos Macacos: A Origem', 'Rupert Wyatt', '2011-01-01', 10.0, '2026-05-26 00:16:22', '2026-05-26 00:16:22');
+(6, 'Planeta dos Macacos: A Origem', 'Rupert Wyatt', '2011-01-01', 10.0, '2026-05-26 00:16:22', '2026-05-26 00:16:22'),
+(7, 'A Hora do Pesadelo', 'James Gunn', '1986-01-12', 10.0, '2026-06-01 22:47:55', '2026-06-01 22:47:55');
 
 --
 -- Índices para tabelas despejadas
@@ -65,7 +66,7 @@ ALTER TABLE `filmes`
 -- AUTO_INCREMENT de tabela `filmes`
 --
 ALTER TABLE `filmes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
